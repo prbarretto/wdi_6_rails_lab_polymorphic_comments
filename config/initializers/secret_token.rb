e15ed079e2be31d8fe153c8047c93af493c2df2a
David Fisher <tibbon@gmail.com>
2014-02-11 12:54:06 -0500

    rails new PolyLab --database=postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolyLab::Application.config.secret_key_base = '64ef18505e6aed929ddcbba52f14306ac4724e8e0a60060dd3fdd7692bab69a0a972438e64b2f5d2985731a372bdb31a4e73e2987b5dcc8bf9857b2251f7ba69'
